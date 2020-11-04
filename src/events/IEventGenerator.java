0d5a7a37423f5b577c54e6afb1c3bc5d473d756a
unknown <vuminhhieu1311@gmail.com>
2020-11-04 11:03:51 +0700

    forth commit

--- src/events/IEventGenerator.java
+++
@@ -1,9 +0,0 @@
-package events;
-
-import infrastructure.state.State;
-
-public interface IEventGenerator {
-	void setState(State state);
-
-	State getState();
-}
--- src/network/entities/TypeOfHost.java
+++
@@ -1,5 +0,0 @@
-package network.entities;
-
-public enum TypeOfHost {
-	Source, Destination, Mix;
-}
